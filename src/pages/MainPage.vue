9a6bc92747ea946b8da60ebb20afd696b6143487
pulna <natasyo23@gmail.com>
2024-11-03 22:33:58 +0300

    fix

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -2,16 +2,6 @@
 import ResumeCardsGrid from "@/components/ResumeCard/ResumeCardsGrid.vue";
 import store from "@/store";
 
-const cards = [
-  { id: 1 },
-  { id: 2 },
-  { id: 3 },
-  { id: 4 },
-  { id: 5 },
-  { id: 6 },
-  { id: 7 },
-  { id: 8 },
-];
 const resumeList = store.getters.resumeAll;
 console.log(resumeList);
 </script>
